444b9eb36ad180c2b6850a4b5b41d56364417cbc
Jiahui Xia <xiajiahui2002@163.com>
2022-05-07 15:19:16 -0400

    update somthing

--- code/System-Dev/app/views/Booking/index.php
+++ code/System-Dev/app/views/Booking/index.php
@@ -1,4 +1,11 @@
 <?php include APPROOT.'/views/includes/header.php'; ?>
 This is the Booking Page
-
+<style>
+    .book {
+        height: 100%;
+        width: 100%;
+        background: url("<?php echo URLROOT; ?>/public/images/booking.png") no-repeat center;
+        background-size: cover;
+    }
+</style>
 <?php include APPROOT . '/views/includes/footer.php'; ?>
