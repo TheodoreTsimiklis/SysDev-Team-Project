2db78ad9cc7a92bfd353565e2a9fb923158a081f
TheodoreTsimiklis <70531739+TheodoreTsimiklis@users.noreply.github.com>
2022-05-07 14:40:15 -0400

    added some backend to about us

--- code/System-Dev/app/models/aboutUsModel.php
+++ code/System-Dev/app/models/aboutUsModel.php
@@ -4,8 +4,25 @@
         public function __construct(){
             $this->db = new Model;
         }
+
         public function getServices(){
             $this->db->query("SELECT * FROM service");
             return $this->db->getResultSet();
         }
+
+        public function getServiceImage($service_id) {
+            this->db->query("SELECT service_image FROM service WHERE ID = :service_id");
+            $this->db->bind(':service_id',$service_id);
+            return $this->db->getSingle();
+
+        }
+
+        public function getServiceName() {
+
+        }
+
+        public function getServiceDescription() {
+
+        }
+    }
 ?>
---
+++ code/System-Dev/app/models/contactModel.php
@@ -0,0 +1,26 @@
+<?php
+
+    class contactModel{
+        public function __construct(){
+            $this->db = new Model;
+        }
+    
+        public function createQuestion($data) {
+            $this->db->query("INSERT INTO questionnotloggedin (first_name, last_name, phone, email, question) VALUES (:first_name, :last_name, :phone, :email, :question)");
+            $this->db->bind(":first_name", $data['first_name']);
+            $this->db->bind(":last_name", $data['last_name']);
+            $this->db->bind(":phone", $data['phone']);
+            $this->db->bind(":email", $data['email']);
+            $this->db->bind(":question", $data['question']);
+    
+            return $this->db->execute();
+        }
+
+        public function loginQuestion($data) {
+            $this->db->query("INSERT INTO question (question_description) VALUES (:question_description)");
+            $this->db->bind(":question_description", $data['question_description']);
+    
+            return $this->db->execute();
+        }
+    }
+?>
